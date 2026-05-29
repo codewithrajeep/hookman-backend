f94b3c21035c512f84a0c7a0951919f6c7c474cc
Rajeep-Gadal <rajeepgadal@gmail.com>
2026-05-29 12:53:40 +0545

    chore: add eslint flat config for typescript

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: ["dist/**", "node_modules/**", "src/generated/**"],
+  },
+];
